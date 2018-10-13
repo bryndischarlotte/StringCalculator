a2497df9cb1b8601059f29d285bb30e52fb0b210
Bryllan <bryndis14@ru.is>
2018-10-13 20:17:51 +0000

    Implemented for one number

--- StringCalculator.js
+++ StringCalculator.js
@@ -4,6 +4,9 @@ function add(number) {
 	if(number == "") {
 		return 0;
 	}
+	else {
+		return parseInt(number);
+	}
 }
 
 module.exports = add; 
--- StringCalculator.test.js
+++ StringCalculator.test.js
@@ -1,7 +1,11 @@
 // StringCalculator.test.js
 
 const add = require("./StringCalculator");
-
+// Empty test
 it("Should return zero on an empty string", () => {
 	expect(add("")).toBe(0);
+});
+// Single digit test
+it("Should return a number when only one number is in the string", () => {
+	expect(add("1")).toBe(1);
 });
